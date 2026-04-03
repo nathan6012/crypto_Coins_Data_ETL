1c514c6713197035e5815b4025516c42132becc5
shamolaETl <shamolanathan0@gmail.com>
2026-04-03 22:03:48 +0300

    test12

--- tests/test_extract_data.py
+++ tests/test_extract_data.py
@@ -4,18 +4,17 @@ import respx
 import httpx
 
 
+
 @pytest.mark.asyncio
 @respx.mock
-async def test_extract_api_data_success():
-    # Mock the endpoint (ignore query string)
-  respx.get("https://api.coingecko.com/api/v3/coins/markets").mock(
-        return_value=httpx.Response(200, json=[{"id": "bitcoin"}])
-    )
+async def test_extract_api_data_success(mocker):
+    # Mock the API key
+  mocker.patch("os.getenv", return_value="fake_api_key")
     
-    # Call your async function
-  data = await extract_api_data()
+    # Mock the HTTP request
+  respx.get("https://api.coingecko.com/api/v3/coins/markets").mock(
+     return_value=httpx.Response(200, json=[{"id": "bitcoin"}])
+    )
 
-    # Assertions
-  assert isinstance(data, list)
-  assert data[0]["id"] == "bitcoin"
-
+  result = await extract_api_data()
+  assert result[0]["id"] == "bitcoin"
